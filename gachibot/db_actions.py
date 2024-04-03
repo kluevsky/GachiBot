10e1009e1670aa79969142f931f22c8e42cad302
kluevsky <kluevsky@protonmail.com>
2024-04-03 20:46:43 +0400

    Added database for favorites
    Implemented functions for database creation and filling by songs from radio

---
+++ gachibot/db_actions.py
@@ -0,0 +1,130 @@
+from dotenv import dotenv_values
+import psycopg2
+from datetime import datetime, timezone
+
+settings = dotenv_values(".env")
+
+DB_SERVER = settings["DB_SERVER"]
+DB_PORT = settings["DB_PORT"]
+DB_USER = settings["DB_USER"]
+DB_PASSWORD = settings["DB_PASSWORD"]
+DB_NAME = settings["DB_NAME"]
+
+def create_db():
+    try:
+        #Create database
+        conn = psycopg2.connect(
+            database="postgres",
+            host=DB_SERVER,
+            port=DB_PORT,
+            user=DB_USER,
+            password=DB_PASSWORD,
+            connect_timeout=5
+        )
+        conn.autocommit = True
+        cursor = conn.cursor()
+        sql = f"CREATE database {DB_NAME}"
+        cursor.execute(sql)
+        conn.close()
+
+        #Create tables
+        conn = psycopg2.connect(
+            database=DB_NAME,
+            host=DB_SERVER,
+            port=DB_PORT,
+            user=DB_USER,
+            password=DB_PASSWORD,
+            connect_timeout=5
+        )
+        conn.autocommit = True
+        # cursor = conn.cursor()
+        with conn.cursor() as cursor:
+            cursor.execute(open("./sql/db.sql", "r").read())
+        conn.close()
+        print("Database created successfully")
+        return True
+    except:
+        print("Unable to create database")
+        return False
+
+
+def check_db():
+    # обработать ошибку коннекта
+    try:
+        conn = psycopg2.connect(
+            database="postgres",
+            host=DB_SERVER,
+            port=DB_PORT,
+            user=DB_USER,
+            password=DB_PASSWORD,
+            connect_timeout=5
+        )
+        cursor = conn.cursor()
+        cursor.execute(f"SELECT datname FROM pg_catalog.pg_database WHERE datname = '{DB_NAME}'")
+        result = cursor.fetchone()
+        conn.close()
+        if not result:
+            return False
+        else:
+            return True
+    except:
+        return False
+
+
+def update_song_list(songs):
+    conn = psycopg2.connect(
+        database=DB_NAME,
+        host=DB_SERVER,
+        port=DB_PORT,
+        user=DB_USER,
+        password=DB_PASSWORD
+    )
+    conn.autocommit = True
+    cursor = conn.cursor()
+    values = ','.join(cursor.mogrify("(%s,%s,%s)", song).decode('utf-8') for song in songs)
+    cursor.execute(f"INSERT INTO songs VALUES {values} ON CONFLICT (id) DO NOTHING")
+
+    song_list_update_time = get_song_list_update_time()
+    current_datetime = datetime.now(timezone.utc)
+    if not song_list_update_time:
+        query = f"INSERT INTO settings VALUES ('{current_datetime}')"
+    else:
+        query = f"UPDATE settings SET last_update = '{current_datetime}'"
+    cursor.execute(query)
+    print("Songs added: " + str(len(songs)))
+    conn.close()
+    return
+
+
+def get_song_list_update_time():
+    conn = psycopg2.connect(
+        database=DB_NAME,
+        host=DB_SERVER,
+        port=DB_PORT,
+        user=DB_USER,
+        password=DB_PASSWORD
+    )
+    cursor = conn.cursor()
+    cursor.execute(f"SELECT last_update FROM settings")
+    result = cursor.fetchone()
+    conn.close()
+    if not result:
+        return None
+    else:
+        return result[0]
+
+
+def get_favorites(cid):
+    return
+
+
+def add_favorites(cid, song):
+    return
+
+
+def delete_favorites(cid, song):
+    return
+
+
+def search_song(search_string):
+    return
--- gachibot/main.py
+++ gachibot/main.py
@@ -1,27 +1,38 @@
 import requests
 import telebot
+import json
 from telebot import types
 from dotenv import dotenv_values
+from enum import Enum, auto
+from datetime import datetime, timedelta, timezone
+from db_actions import *
+
 
 settings = dotenv_values(".env")
 
 TOKEN = settings["TOKEN"]
 BASE_URL = settings["BASE_URL"]
+FAVORITES_FILE = settings["FAVORITES_FILE"]
 
 button_labels = {
     "next_track": "Какой следующий трек?",
     "request_track": "Заказать трек",
-    "go_back": "🔙 Назад"
+    "go_back": "🔙 Назад",
+    "star": "⭐"
 }
 
 endpoints = {
     "request": "api/station/1/request/",
     "now_playing": "api/nowplaying/gachibass_radio",
     "search": "api/station/1/requests"
-
 }
 
-user_step = {}
+class SongOperation(Enum):
+    request = auto()
+    add_favorite = auto()
+
+db_exists = bool()
+user_step = dict()
 main_menu = types.ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=False)
 
 bot = telebot.TeleBot(TOKEN, parse_mode=None)
@@ -52,32 +63,35 @@ def handle_search(message):
     if not result["rows"]:
         bot.send_message(message.chat.id, "Ниче не найдено, повтори")
     else:
-        markup = types.InlineKeyboardMarkup()
+        markup = types.InlineKeyboardMarkup(row_width=5)
         for song in result["rows"]:
-            markup.add(types.InlineKeyboardButton(text=song["song"]["title"], callback_data=song["request_id"]))
+            markup.row(
+                types.InlineKeyboardButton(
+                    text=song["song"]["title"], 
+                    callback_data=get_song_callback_string(
+                        SongOperation.request.value, 
+                        song["request_id"]
+                    )
+                ),
+                types.InlineKeyboardButton(
+                    text=button_labels["star"], 
+                    callback_data=get_song_callback_string(
+                        SongOperation.add_favorite.value, 
+                        song["song"]["id"]
+                    )
+                )
+            )
         bot.send_message(message.chat.id, "Выбирай", reply_markup=markup)
 
 
 @bot.callback_query_handler(func=lambda call:True)
-def request_song(callback):
+def handle_song_callback(callback):
     cid = callback.message.chat.id
-    endpoint = endpoints["request"]
-    headers = {
-        "Accept": "application/json",
-        "Accept-Language": "ru"
-    }
-    try:
-        request = requests.post(BASE_URL + endpoint + callback.data, headers=headers)
-        response = request.json()
-    except requests.exceptions.RequestException as e:
-        bot.send_message(cid, "КРИТИЧЕСКИЙ СБОЙ")
-        user_step[cid] = 0
-        return
-    
-    if response["success"] == True:
-        open_main_menu(cid, 0, "ТРЕК ЗАКАЗАН", button_labels["next_track"], button_labels["request_track"])
-    else:
-        bot.send_message(cid, response["formatted_message"])
+    callback_json = json.loads(callback.data)
+    if callback_json["operation"] == SongOperation.request.value:
+        request_song(callback)
+    elif callback_json["operation"] == SongOperation.add_favorite.value:
+        add_favorite(callback)
 
 
 def get_next_track():
@@ -105,6 +119,39 @@ def search_song(search_phrase):
     return response
 
 
+def get_song_callback_string(operation, song_id):
+    dict = {"operation": operation, "song_id": song_id}
+    return str(dict).replace("'", '"')
+
+
+def request_song(callback):
+    #передавать cid сюда
+    cid = callback.message.chat.id
+    callback_json = json.loads(callback.data)
+    endpoint = endpoints["request"]
+    headers = {
+        "Accept": "application/json",
+        "Accept-Language": "ru"
+    }
+    try:
+        request = requests.post(BASE_URL + endpoint + callback_json["song_id"], headers=headers)
+        response = request.json()
+    except:
+        bot.send_message(cid, "КРИТИЧЕСКИЙ СБОЙ")
+        user_step[cid] = 0
+        return
+    
+    if response["success"] == True:
+        open_main_menu(cid, 0, "ТРЕК ЗАКАЗАН", button_labels["next_track"], button_labels["request_track"])
+    else:
+        bot.send_message(cid, response["formatted_message"])
+
+
+def add_favorite(callback):
+    #передавать cid сюда
+    return
+
+
 def get_user_step(cid):
     if cid in user_step:
         return user_step[cid]
@@ -117,6 +164,44 @@ def open_main_menu(cid, step, message, *buttons):
     main_menu.keyboard.clear()
     main_menu.row(*buttons)
     bot.send_message(cid, message, reply_markup=main_menu)
+
+
+def get_all_songs():
+    songs = list()
+    endpoint = endpoints["search"]
+    params = {
+        "internal": "true",
+        "rowCount": 100,
+        "current": 1,
+        "flushCache": "true",
+        "sortOrder": "ASC"
+    }
+    request = requests.get(BASE_URL + endpoint, params)
+    response = request.json()
+    total_pages = response["total_pages"]
+    for page in range(1, total_pages + 1):
+        params["current"] = page
+        request = requests.get(BASE_URL + endpoint, params)
+        response = request.json()
+        for song in response["rows"]:
+            songs.append((song["song"]["id"], song["song"]["title"], song["request_id"]))
+    return songs
+
+db_exists = check_db()
+if not db_exists:
+    db_exists = create_db()
+
+if db_exists:
+    song_list_update_time = get_song_list_update_time()
+    print(datetime.now(timezone.utc))
+    print(song_list_update_time)
+    if not song_list_update_time or (datetime.now(timezone.utc) - timedelta(hours=24) >= song_list_update_time):
+        songs = get_all_songs()
+        update_song_list(songs)
+    else:
+        print("Last update: " + str(song_list_update_time))
+else:
+    print("Работаем без базы")
 
 
 bot.infinity_polling()
